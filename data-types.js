8f42c25f2535937642cf03c04e0cafa53a976420
Fillinius <113914362+Fillinius@users.noreply.github.com>
2022-11-08 19:42:38 +0300

    Add files via upload

---
+++ data-types.js
@@ -0,0 +1,34 @@
+//number
+const age = 40
+console.log('age:', age, typeof age)
+
+//string
+const fistName = 'Vladimir'
+console.log('fistMame:', fistName, typeof fistName)
+
+//boolean
+const children = true
+console.log('Children:', children, typeof children)
+
+//null
+const bike = null
+console.log('bike', bike)
+
+//undefined
+let motoBike = undefined
+console.log('motoBike', typeof motoBike)
+
+//object
+const data = {
+  age: 40,
+  fistName: 'Vlsdimir',
+}
+console.log(typeof data)
+
+//bigint
+const ageBig = 10n
+console.log(typeof ageBig)
+
+//symbol
+const sym = Symbol('id')
+console.log(typeof sym)
